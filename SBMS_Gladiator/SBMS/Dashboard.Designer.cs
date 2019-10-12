f3f0e50f96576d800d787d11bc86043e3bd1b012
delwar-hussain <cse.delwar@gmail.com>
2019-10-12 19:00:51 +0600

    text_file_added

---
+++ SBMS_Gladiator/SBMS/Dashboard.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace SBMS
+{
+    partial class Dashboard
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.productCatergory = new System.Windows.Forms.Button();
+            this.newProduct = new System.Windows.Forms.Button();
+            this.newCustomer = new System.Windows.Forms.Button();
+            this.newPurchase = new System.Windows.Forms.Button();
+            this.newSupplier = new System.Windows.Forms.Button();
+            this.newSales = new System.Windows.Forms.Button();
+            this.newOrder = new System.Windows.Forms.Button();
+            this.searchStock = new System.Windows.Forms.Button();
+            this.searchSalesReport = new System.Windows.Forms.Button();
+            this.newStock = new System.Windows.Forms.Button();
+            this.searchPurchaseReport = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // productCatergory
+            // 
+            this.productCatergory.AllowDrop = true;
+            this.productCatergory.Location = new System.Drawing.Point(95, 73);
+            this.productCatergory.Name = "productCatergory";
+            this.productCatergory.Size = new System.Drawing.Size(155, 128);
+            this.productCatergory.TabIndex = 0;
+            this.productCatergory.Text = "Product Catergory";
+            this.productCatergory.UseVisualStyleBackColor = true;
+            // 
+            // newProduct
+            // 
+            this.newProduct.AllowDrop = true;
+            this.newProduct.Location = new System.Drawing.Point(286, 73);
+            this.newProduct.Name = "newProduct";
+            this.newProduct.Size = new System.Drawing.Size(155, 128);
+            this.newProduct.TabIndex = 1;
+            this.newProduct.Text = "New Product";
+            this.newProduct.UseVisualStyleBackColor = true;
+            // 
+            // newCustomer
+            // 
+            this.newCustomer.AllowDrop = true;
+            this.newCustomer.Location = new System.Drawing.Point(473, 73);
+            this.newCustomer.Name = "newCustomer";
+            this.newCustomer.Size = new System.Drawing.Size(155, 128);
+            this.newCustomer.TabIndex = 2;
+            this.newCustomer.Text = "New Customer";
+            this.newCustomer.UseVisualStyleBackColor = true;
+            // 
+            // newPurchase
+            // 
+            this.newPurchase.AllowDrop = true;
+            this.newPurchase.Location = new System.Drawing.Point(844, 73);
+            this.newPurchase.Name = "newPurchase";
+            this.newPurchase.Size = new System.Drawing.Size(155, 128);
+            this.newPurchase.TabIndex = 4;
+            this.newPurchase.Text = "Purchase";
+            this.newPurchase.UseVisualStyleBackColor = true;
+            // 
+            // newSupplier
+            // 
+            this.newSupplier.AllowDrop = true;
+            this.newSupplier.Location = new System.Drawing.Point(657, 73);
+            this.newSupplier.Name = "newSupplier";
+            this.newSupplier.Size = new System.Drawing.Size(155, 128);
+            this.newSupplier.TabIndex = 3;
+            this.newSupplier.Text = "New Supplier";
+            this.newSupplier.UseVisualStyleBackColor = true;
+            // 
+            // newSales
+            // 
+            this.newSales.AllowDrop = true;
+            this.newSales.Location = new System.Drawing.Point(411, 213);
+            this.newSales.Name = "newSales";
+            this.newSales.Size = new System.Drawing.Size(155, 128);
+            this.newSales.TabIndex = 7;
+            this.newSales.Text = "Sales";
+            this.newSales.UseVisualStyleBackColor = true;
+            // 
+            // newOrder
+            // 
+            this.newOrder.AllowDrop = true;
+            this.newOrder.Location = new System.Drawing.Point(95, 238);
+            this.newOrder.Name = "newOrder";
+            this.newOrder.Size = new System.Drawing.Size(155, 128);
+            this.newOrder.TabIndex = 5;
+            this.newOrder.Text = "Order";
+            this.newOrder.UseVisualStyleBackColor = true;
+            // 
+            // searchStock
+            // 
+            this.searchStock.AllowDrop = true;
+            this.searchStock.Location = new System.Drawing.Point(33, 46);
+            this.searchStock.Name = "searchStock";
+            this.searchStock.Size = new System.Drawing.Size(155, 128);
+            this.searchStock.TabIndex = 8;
+            this.searchStock.Text = "Stock";
+            this.searchStock.UseVisualStyleBackColor = true;
+            // 
+            // searchSalesReport
+            // 
+            this.searchSalesReport.AllowDrop = true;
+            this.searchSalesReport.Location = new System.Drawing.Point(224, 46);
+            this.searchSalesReport.Name = "searchSalesReport";
+            this.searchSalesReport.Size = new System.Drawing.Size(155, 128);
+            this.searchSalesReport.TabIndex = 9;
+            this.searchSalesReport.Text = "Sales Report";
+            this.searchSalesReport.UseVisualStyleBackColor = true;
+            // 
+            // newStock
+            // 
+            this.newStock.AllowDrop = true;
+            this.newStock.Location = new System.Drawing.Point(286, 238);
+            this.newStock.Name = "newStock";
+            this.newStock.Size = new System.Drawing.Size(155, 128);
+            this.newStock.TabIndex = 6;
+            this.newStock.Text = "New Stock";
+            this.newStock.UseVisualStyleBackColor = true;
+            // 
+            // searchPurchaseReport
+            // 
+            this.searchPurchaseReport.AllowDrop = true;
+            this.searchPurchaseReport.Location = new System.Drawing.Point(411, 46);
+            this.searchPurchaseReport.Name = "searchPurchaseReport";
+            this.searchPurchaseReport.Size = new System.Drawing.Size(155, 128);
+            this.searchPurchaseReport.TabIndex = 10;
+            this.searchPurchaseReport.Text = "Purchase Report";
+            this.searchPurchaseReport.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.newSales);
+            this.groupBox1.Location = new System.Drawing.Point(62, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(967, 364);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "New Entry";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.searchStock);
+            this.groupBox2.Controls.Add(this.searchSalesReport);
+            this.groupBox2.Controls.Add(this.searchPurchaseReport);
+            this.groupBox2.Location = new System.Drawing.Point(62, 418);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(967, 214);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Search";
+            // 
+            // Dashboard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1090, 670);
+            this.Controls.Add(this.newStock);
+            this.Controls.Add(this.newSupplier);
+            this.Controls.Add(this.newPurchase);
+            this.Controls.Add(this.newOrder);
+            this.Controls.Add(this.newProduct);
+            this.Controls.Add(this.newCustomer);
+            this.Controls.Add(this.productCatergory);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Dashboard";
+            this.Text = "Dashboard";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button productCatergory;
+        private System.Windows.Forms.Button newProduct;
+        private System.Windows.Forms.Button newCustomer;
+        private System.Windows.Forms.Button newPurchase;
+        private System.Windows.Forms.Button newSupplier;
+        private System.Windows.Forms.Button newSales;
+        private System.Windows.Forms.Button newOrder;
+        private System.Windows.Forms.Button searchStock;
+        private System.Windows.Forms.Button searchSalesReport;
+        private System.Windows.Forms.Button newStock;
+        private System.Windows.Forms.Button searchPurchaseReport;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
---
+++ SBMS_Gladiator/SBMS/Login.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace SBMS
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.userTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(215, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "User";
+            // 
+            // userTextBox
+            // 
+            this.userTextBox.Location = new System.Drawing.Point(334, 111);
+            this.userTextBox.Name = "userTextBox";
+            this.userTextBox.Size = new System.Drawing.Size(217, 31);
+            this.userTextBox.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(215, 168);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Password";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(334, 165);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(217, 31);
+            this.textBox1.TabIndex = 1;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(438, 247);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(110, 47);
+            this.loginButton.TabIndex = 2;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = true;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.userTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox userTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button loginButton;
+    }
+}
+
